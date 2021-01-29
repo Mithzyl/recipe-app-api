576886bd3079bdf5073003db85ce4c48bc5ae5d1
Mithzyl <huyun1645@163.com>
2021-01-29 15:57:48 +0800

    Added recipe model

--- app/core/admin.py
+++ app/core/admin.py
@@ -3,6 +3,7 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext as _
 from . import models
 
+
 class UserAdmin(BaseUserAdmin):
     ordering = ['id']
     list_display = ['email', 'name']
@@ -26,3 +27,4 @@ class UserAdmin(BaseUserAdmin):
 admin.site.register(models.User, UserAdmin)
 admin.site.register(models.Tag)
 admin.site.register(models.Ingredient)
+admin.site.register(models.Recipe)
